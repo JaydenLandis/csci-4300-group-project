4bfee243399d761d268c4ebb6ccbb0f240a8dac9
supitsadrian <aim39628@uga.edu>
2025-04-15 13:25:29 -0400

    added about page but no director from home page to it

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image"
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
+    <div className ="p-5 bg-dark-subtle">
+      <div className = "d-flex flex-row bg-white p-3 m-5 border bd-highlight rounded shadow-lg">
+      
+        <Image src="/assets/foundingfathers.jpg"
+        alt = "Picture of AutoFlash's founders: Adrian Morales, Jayden Landis, Sam Pinho, and Jackson Barlow"
+        className="d-block w-100 rounded"
+        width={300}
+        height={700}/>
+        <h3 className="p-5 w-75 text-center">We founded AutoFlash to increase studying efficiency.
+        We used INSERTCOMPANYHERE's API to take any quizzes or tests taken and graded,
+        and turn them into studyable flashcards for future quizzes. We partnered in March of 2025
+        as a group to originally create a final project for our Web Development class.
+        However, there is always more in the future to improve with AutoFlash!
+        </h3>
+      
+      </div>
     </div>
   );
 };
