d243df5f021bbf81f0d16fb785785651162fdfce
supitsadrian <aim39628@uga.edu>
2025-04-17 19:51:07 -0400

    user-authentication/alpha : wrote structure of authentication... still needs applications in components and debugging

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,12 +2,25 @@ import connectMongoDB from "../../../../config/mongodb";
 import User from "../../../models/userSchema";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(request: NextRequest) {
   const { username, password} = await request.json();
   await connectMongoDB();
-  await User.create({ username, password});
-  return NextResponse.json({ message: "Item added successfully" }, { status: 201 });
+  const hashedPassword = await bcrypt.hash(password,5);
+  const newUser = {
+    username,
+    password: hashedPassword
+  }
+  try {
+   await User.create(newUser);
+  }
+  catch (e:any){
+    return NextResponse.json({message: "Item could not be added"},{status: 401});
+  }
+  finally{
+    return NextResponse.json({ message: "Item added successfully" }, { status: 201 });
+  }
 }
 
 export async function GET() {
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,50 @@
+'use server'
 import React from "react";
+import { FormEvent } from "react";
+import Router from "next/router";
+import {signIn, signOut} from "../auth";
+
+export async function doLogOut() {
+  await signOut({redirectTo: "/"});
+}
+
+export async function doCredentialLogin(formData:FormData): Promise<any> {
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
+
+  try {
+    const response = await signIn("credentials", {
+     username,
+     password,
+     redirect: false,
+    });
+    return response;
+  } catch (e: any) {
+    throw e;
+  }
+}
+async function onSubmit(event: FormEvent<HTMLFormElement>):Promise<void> {
+  event.preventDefault();
+
+  try {
+    const formData = new FormData(event.currentTarget);
+    const response = await doCredentialLogin(formData);
+
+    if (response?.error) {
+      console.error(response.error);
+      setError(response.error.message || "An error occured");
+    } else{
+      Router.push("/");
+    }
+  } catch (e:any) {
+    console.error(e);
+    setError("Check your Credentials");
+  }
+}
+
 
 const Login = () => {
+
   return (
     <div className="flex flex-col items-center justify-content-center text-center h-100 bg-gray-100 p-5">
       <h1 className="text-3xl font-bold mb-2">
